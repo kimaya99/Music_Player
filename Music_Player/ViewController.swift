3d4727391393af164a2464b56a6a2c88d4491662
kimaya99 <kimayadesai@Kimayas-MacBook-Pro.local>
2017-10-11 00:28:52 -0700

    first commit

---
+++ Music_Player/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  Music_Player
+//
+//  Created by Kimaya Desai on 10/10/17.
+//  Copyright © 2017 SDSU. All rights reserved.
+//
+
+import UIKit
+import MediaPlayer
+
+
+class ViewController: UIViewController {
+
+    var musicPlayer = MPMusicPlayerController.applicationMusicPlayer
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func genreButtonTapped(_ sender: UIButton) {
+        MPMediaLibrary.requestAuthorization { (status) in
+            if status == .authorized{
+                self.playGenre(genre: sender.currentTitle!)
+            }
+        }
+    }
+    
+   
+    
+    @IBAction func stopTapp(_ sender: UIButton) {
+        musicPlayer.stop()
+    }
+    
+   
+   
+    
+  
+    
+    
+    @IBAction func nextTapp(_ sender: UIButton) {
+        musicPlayer.skipToNextItem()
+    }
+    
+    func playGenre(genre: String){
+        musicPlayer.stop()
+        
+        let query = MPMediaQuery()
+        let predicate = MPMediaPropertyPredicate(value:genre,forProperty:MPMediaItemPropertyGenre)
+        query.addFilterPredicate(predicate)
+        
+        musicPlayer.setQueue(with: query)
+        musicPlayer.shuffleMode = .songs
+        musicPlayer.play()
+        
+        
+    }
+
+}
+
